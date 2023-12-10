75100a33080153bb5adc584c7c0642cdc4a36cd7
Ahammad204 <Ahammadcode@gmail.com>
2023-12-10 22:26:02 +0600

    Added navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -5,7 +6,7 @@ const Navbar = () => {
 
     const navLink = <>
     
-    <li><a>Submenu 2</a></li>
+    <li  className="m-2 md:mr-5 text-lg hover:text-[#f04336] hover:underline"><NavLink to="/">Home</NavLink></li>
 
     </>
 
@@ -16,20 +17,18 @@ const Navbar = () => {
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
              {navLink}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">Ahammad</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className=" menu-horizontal px-1">
           {navLink}
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div>
+       
       </div>
     );
 };
