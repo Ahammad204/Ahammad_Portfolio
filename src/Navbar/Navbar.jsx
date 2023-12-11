1c603344087f9d3e62dfd019c968a1974090f9a2
Ahammad204 <Ahammadcode@gmail.com>
2023-12-11 21:10:27 +0600

    Fixed navbar issue

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -12,25 +12,25 @@ const Navbar = () => {
     </>
 
     return (
-      <div className="navbar  shadow-xl p-4 max-w-screen-xl mx-auto fixed">
-      <div className="navbar-start">
+        <div className="navbar bg-base-100 shadow-xl">
+        <div className="navbar-start">
           <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-              </label>
-              <ul tabIndex={0} className=" menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-500 rounded-box w-52">
-                  {navLink}
-              </ul>
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul tabIndex={0} className=" menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+           {navLink}
+            </ul>
           </div>
-      
-      </div>
-      <div className="navbar-center hidden lg:flex">
+          <a className="btn btn-ghost text-xl">Ahammad</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
           <ul className=" menu-horizontal px-1">
-              {navLink}
+        {navLink}
           </ul>
+        </div>
+     
       </div>
-   
-  </div>
     );
 };
 
